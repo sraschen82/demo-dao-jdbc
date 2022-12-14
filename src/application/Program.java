8dd4ca0df7f90216090d0daea06e8e3f26dd6ca4
Samuel Raschen <sraschen@yahoo.com.br>
2022-12-14 15:02:31 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -26,6 +27,13 @@ public class Program {
 		System.out.println("\n=== TEST 3: seller findAll ===");
 		List<Seller> list2 = sellerDao.findAll();
 		list2.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = "+newSeller.getId());
+		
+		
 	}
 
 }
